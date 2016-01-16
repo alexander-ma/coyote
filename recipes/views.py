026ffcbfcd262288a33694ab33e8a0b4d79fc358
Max Wang <maxwang051@gmail.com>
2016-01-16 00:23:54 -0600

    added url for home page and included recipes url, made first api call for recipe search

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+import unirest
 
-# Create your views here.
+def home_page(request):
+
+    return HttpResponse('<h1>Home Page</h1>')
+
+def recipe_list(request):
+    response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5",
+      headers={
+        "X-Mashape-Key": "QN5CLcAiQXmshOrib4vl7QifQAPjp1MjXoijsnsKdgztp93FnI",
+        "Accept": "application/json"
+      }
+    )
+
+    context = {
+        'response': response
+    }
+
+    return render(request, 'recipes_list.html', context)
